95d90ab2d9593a9efb8924fc4bd7d0e65595585f
Tomasz Ras <tomaszras@live.com>
2019-03-31 18:43:39 +0200

    testing btn

--- public/client.js
+++ public/client.js
@@ -1,9 +1,8 @@
 TrelloPowerUp.initialize({
-  'board-buttons': function(t, options){
+  'card-buttons': function(t, options){
     return [{
-      text: 'Inspiring Boards',
-      url: 'https://trello.com/inspiration',
-      target: 'Inspiring Boards'
+      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+      text: 'Estimate Size',
     }];
-  }
+  },
 });
